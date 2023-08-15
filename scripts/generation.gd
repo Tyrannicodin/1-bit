f68688cf015c03a39964861ecf649d780d250c58
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2023-08-15 18:32:32 +0100

    Finish room generation, move nightstand to furniture

--- scripts/generation.gd
+++ scripts/generation.gd
@@ -1,12 +1,13 @@
 extends Node3D
 
-@export var recursion_depth = 1
+@export var recursion_depth := 3
 
-var generator = RandomNumberGenerator.new()
-var rooms = {}
-var category_info = {}
-var entrance = []
-var end = []
+var generator := RandomNumberGenerator.new()
+var rooms := {}
+var category_info := {}
+var entrance:Array = []
+var end:Array = []
+var room_boundings:Array[AABB] = []
 
 func random_choice(array:Array):
 	var idx = generator.randi_range(0, len(array)-1)
@@ -47,7 +48,55 @@ func load_rooms():
 	end = rooms["end"]
 	rooms.erase("end")
 
-func generate_new_branch(base_room: Node):
+func try_place_room(door:Node3D, new_door:Node3D, new_room:Node3D):
+	if not door.is_inside_tree():
+		print(door)
+	if not new_door.is_inside_tree():
+		print(new_door)
+	if not new_room.is_inside_tree():
+		print(new_room)
+	
+	# find the required rotation of the room
+	var door_rotation = door.global_rotation.y + new_door.rotation.y
+	new_room.global_rotation = Vector3.UP * -door_rotation
+	
+	# rotate the door's position to find the proper offset
+	var new_door_position = new_door.position.rotated(Vector3.UP, -door_rotation)
+	
+	# set position so it the two door touches
+	new_room.global_position.x = door.global_position.x - new_door_position.x
+	new_room.global_position.z = door.global_position.z - new_door_position.z
+	
+	var new_area:CollisionShape3D = new_room.get_node("area").get_child(0)
+	var new_bounding = AABB(new_room.global_position, new_area.shape.size)
+	for room in room_boundings:
+		if room.intersects(new_bounding):
+			return false
+	room_boundings.append(new_bounding)
+	return true
+
+func place_end(door):
+	var room_parent = door.get_parent()
+	
+	var available_rooms = end.duplicate()
+	var new_packed_scene = random_choice(end)
+	available_rooms.erase(new_packed_scene)
+	var new_room = new_packed_scene.instantiate()
+	room_parent.add_child(new_room)
+	var placed = try_place_room(door, new_room.get_node("door"), new_room)
+	while not placed:
+		new_room.queue_free()
+		if len(available_rooms) == 0:
+			# TODO: make door get boarded up
+			return
+		new_packed_scene = random_choice(end)
+		available_rooms.erase(new_packed_scene)
+		new_room = new_packed_scene.instantiate()
+		room_parent.add_child(new_room)
+		placed = try_place_room(door, new_room.get_node("door"), new_room)
+	new_room.get_node("door").set_meta("connected", true)
+
+func generate_new_branch(base_room: Node3D):
 	for node in base_room.get_children():
 		if node.name.contains("door"):
 			if node.get_meta("connected", false): # Ignore door if it has connection
@@ -65,25 +114,19 @@ func generate_new_branch(base_room: Node):
 				if child.name.contains("door"):
 					new_room_doors.append(child)
 			var new_chosen_door = random_choice(new_room_doors)
-			new_chosen_door.set_meta("connected", true)
 			
-			# get position from origin of door
-			var door_position = Vector2(door.global_position.x, door.global_position.z)
-			
-			# find the required rotation of the room
-			var door_rotation = door.global_rotation.y + new_chosen_door.rotation.y
-			new_room.global_rotation = Vector3.UP * -door_rotation
-			
-			# rotate the door's position to find the proper offset
-			var new_door_position = new_chosen_door.position.rotated(Vector3.UP, -door_rotation)
-			
-			# set position so it the two door touches
-			new_room.global_position.x = door_position.x - new_door_position.x
-			new_room.global_position.z = door_position.y - new_door_position.z
-			
-			print(new_room.get_node("area").has_overlapping_areas())
-			
+			var placed = try_place_room(door, new_chosen_door, new_room)
+			while not placed:
+				new_room_doors.erase(new_chosen_door)
+				if len(new_room_doors) == 0:
+					new_room.queue_free()
+					place_end(door)
+					placed = true
+					continue
+				new_chosen_door = random_choice(new_room_doors)
+				placed = try_place_room(door, new_chosen_door, new_room)
 			# delete door so only the new one remains
+			new_chosen_door.set_meta("connected", true)
 			door.queue_free()
 	# output for next level of recursion
 	return base_room.get_children()
@@ -103,6 +146,9 @@ func _ready():
 	# First create a base child
 	var base_room = random_choice(entrance).instantiate()
 	add_child(base_room)
+	var base_area:CollisionShape3D = base_room.get_node("area").get_child(0)
+	var base_aabb = AABB(base_room.global_position, base_area.shape.size)
+	room_boundings.append(base_aabb)
 	
 	for i in recursion_depth:
 		var new_nodes = generate_new_branch(base_room)
