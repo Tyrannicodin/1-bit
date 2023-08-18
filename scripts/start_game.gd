55efbfba0515f043d9a1f335728c1f5e9eccc86c
Benji42 <63879236+Benji42@users.noreply.github.com>
2023-08-18 18:32:13 -0400

    Link up buttons to title

---
+++ scripts/start_game.gd
@@ -0,0 +1,8 @@
+extends Button
+@onready var subviewport = $"../../../.."
+
+func _ready():
+	self.pressed.connect(self._button_pressed)
+
+func _button_pressed():
+	get_tree().change_scene_to_file("res://scenes/generation_test.tscn")
