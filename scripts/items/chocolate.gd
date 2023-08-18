735ec93fed390db76e6a739b3b10332f2301628f
Lunarmagpie <bambolambo0@gmail.com>
2023-08-18 17:51:19 -0400

    add missing files

---
+++ scripts/items/chocolate.gd
@@ -0,0 +1,16 @@
+extends "item_3D.gd"
+
+func focus():
+	$battery.set_surface_override_material(0, item_focused_material)
+	
+func unfocus():
+	$battery.set_surface_override_material(0, item_material)
+
+func get_texture() -> Texture2D:
+	return load("res://assets/items/battery.png")
+
+func get_item_name() -> String:
+	return "CHOCOLATE"
+
+func get_description() -> String:
+	return "Chocolate: Pause Battery Draining"
